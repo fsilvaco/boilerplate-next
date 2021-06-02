7213f76119d65c8cc55a738338609bcdac4a5c59
fsilvaco <contatoflaviodev@gmail.com>
2021-06-02 20:51:07 -0300

    Add @storybook/react 6.2.9 and add an stories

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,13 @@
-export const Main = () => {
+import * as S from './styles'
+
+export const Main = ({
+  title = 'Boileplate',
+  description = 'Edit the Main component'
+}) => {
   return (
-    <main>
-      <h1>Edit main component</h1>
-    </main>
+    <S.Wrapper>
+      <p>{title}</p>
+      <p>{description}</p>
+    </S.Wrapper>
   )
 }
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `
